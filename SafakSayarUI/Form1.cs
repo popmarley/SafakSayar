7c6328f2569221a1d8a97281942b16fc95d16a62
pop marley <hsynozguven@hotmail.com>
2023-03-30 22:41:42 +0300

    Add project files.

---
+++ SafakSayarUI/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SafakSayarUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DateTime tarih1 = dateTimePicker1.Value;
+
+            // İkinci tarihi al
+            DateTime tarih2 = dateTimePicker2.Value;
+
+            // Gün sayısını hesapla
+            TimeSpan fark = tarih2 - tarih1;
+            int gunSayisi = fark.Days;
+
+            // Label'da göster
+            label1.Text = $"Atarsa {gunSayisi} gün ";
+        }
+
+        
+    }
+}
